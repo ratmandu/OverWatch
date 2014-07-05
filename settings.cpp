4aca5d2852d1c98b3141e9c002c7142678d29dd2
Justin Richards <ratmandu@gmail.com>
2014-07-05 10:01:25 -0500

    Fleshed out settings wrapper class

--- settings.cpp
+++ settings.cpp
@@ -3,4 +3,15 @@
 Settings::Settings(QObject *parent) :
   QObject(parent)
 {
+  _settings = new QSettings("/sdcard/DashCam/conf", QSettings::NativeFormat);
+}
+
+void Settings::setValue(const QString &key, const QVariant &value)
+{
+  _settings->setValue(key, value);
+}
+
+QVariant Settings::value(const QString &key, const QVariant &defaultValue) const
+{
+  return _settings->value(key, defaultValue);
 }
--- settings.h
+++ settings.h
@@ -2,12 +2,18 @@
 #define SETTINGS_H
 
 #include <QObject>
+#include <QSettings>
 
 class Settings : public QObject
 {
   Q_OBJECT
 public:
   explicit Settings(QObject *parent = 0);
+  Q_INVOKABLE void setValue(const QString &key, const QVariant &value);
+  Q_INVOKABLE QVariant value(const QString &key, const QVariant &defaultValue = QVariant()) const;
+
+private:
+  QSettings &_settings;
 
 signals:
 
